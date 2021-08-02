5655a969beab2b52faddfbfeba88bc6c94f6ba81
chrislucas <christoffer.luccas@gmail.com>
2021-08-02 01:41:51 -0300

    merging e estudos

---
+++ week4/StudiesWeek1/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+m
+=src/com/br/week4/properties/exec/UnstableImmutableProperty.kt,9/3/934d9811012c2025af6e0f648b21d7c6edebc955
+k
+;src/com/br/week4/properties/fields/visibility/CountChars.kt,e/6/e6a008cf773e790b6c72327f5b16cab17d50ce3a
+k
+;src/com/br/week4/properties/SampleBackingFieldVsFunction.kt,9/4/947f454c9f88bf4f3ec6ea394742310af2b11d2c
+_
+/src/com/br/week4/properties/SampleProperties.kt,b/1/b1781fe8cb652fcc7d00150b5e7470a00e6d260c
+^
+.src/com/br/week4/properties/model/Rectangle.kt,6/d/6d01bdda639db32216a46728bb4efb6ea353c425
--- week4/StudiesWeek1/src/com/br/week4/properties/model/Rectangle.kt
+++ week4/StudiesWeek1/src/com/br/week4/properties/model/Rectangle.kt
@@ -3,16 +3,37 @@ package com.br.week4.properties.model
 import kotlin.math.abs
 
 
-const val EPS = 10e-6
+const val EPS = 10e-9
 
-infix fun Double.almostEquals(that: Double):Boolean = abs(this - that) < EPS
+fun Double.almostEquals(that: Double, eps: Double): Boolean = abs(this - that) < eps
 
 /**
- * Exemplo de classe com propriedades que nao utilizando 'field'
+ * Exemplo de classe com propriedades que nao utilizam 'field' s
  * */
-class Rectangle(private val height: Double, private val width: Double) {
+class Rectangle(private val height: Double, private val width: Double, private val eps: Double = EPS) {
     val isSquare: Boolean
         get() {
-            return height almostEquals width
+            return height.almostEquals(width, eps)
         }
+}
+
+private fun test(pair: Pair<Double, Double>) {
+    val rect = Rectangle(pair.first, pair.second)
+    println(rect.isSquare)
+}
+
+fun main() {
+    println(String.format("%.10f", EPS))
+    arrayOf(
+        10.0 to 10.00004,
+        10.0 to 10.00001,
+        10.0 to 10.0001,
+        10.0 to 10.000004,
+        10.0 to 10.00000002,
+        10.0 to 10.000000002,
+        10.0 to 10.5,
+    ).forEach {
+        test(it)
+    }
+
 }
