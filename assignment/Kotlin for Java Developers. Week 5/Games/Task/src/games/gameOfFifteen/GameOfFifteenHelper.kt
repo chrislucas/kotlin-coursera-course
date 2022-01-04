3b9051e999eb28b7610e7902d85b4a1c53f2888c
christoffer lucas <christoffer.santos@mercadolivre.com>
2022-01-04 15:53:59 -0300

    implementando metodo para calcular paridade de uma permutacao

--- assignment/Kotlin for Java Developers. Week 5/Games/Task/src/games/gameOfFifteen/GameOfFifteenHelper.kt
+++ assignment/Kotlin for Java Developers. Week 5/Games/Task/src/games/gameOfFifteen/GameOfFifteenHelper.kt
@@ -11,5 +11,44 @@ package games.gameOfFifteen
  * Thus the initial permutation should be correct.
  */
 fun isEven(permutation: List<Int>): Boolean {
-    TODO()
+    return permutation.isSorted() || permutation.parityOfPermutation()
+}
+
+/*
+    Se o numero de trocas para fazer o vetor voltar ao normal for par
+    a paridade é par do contrário impar
+    https://www.ime.unicamp.br/~marcia/AlgebraLinear/determinantes.html#
+ */
+
+fun <T : Comparable<T>> List<T>.parityOfPermutation(): Boolean {
+    if (this.isEmpty())
+        return false
+
+    val map = mutableMapOf<T, Int>()
+    for (i in this.indices) {
+        for (j in i + 1 until this.size) {
+            if (this[i] > this[j]) {
+                map[this[i]] = map[this[i]]?.plus(1) ?: 1
+            }
+        }
+    }
+    return map.values.sum() and 1 == 0
+}
+
+
+internal fun <T : Comparable<T>> List<T>.isSorted(): Boolean {
+    for (i in 1 until this.size) {
+        if (this[i - 1] > this[i])
+            return false
+    }
+    return true
+}
+
+
+fun main() {
+    println(listOf(4, 2, 1, 3).parityOfPermutation())
+    println(listOf(2, 1, 4, 3).parityOfPermutation())
+    println(listOf(2, 1, 3, 4).parityOfPermutation())
+    println(listOf(4, 1, 2, 3).parityOfPermutation())
+    println(listOf(3, 2, 1, 4).parityOfPermutation())
 }
